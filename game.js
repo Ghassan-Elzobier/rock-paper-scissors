5e981297c5a4dcb7b87ce068f6f6542cfb79ecee
Ghassan-Elzobier <ghassanelzobier@gmail.com>
2025-10-09 11:28:01 +0100

    Add playRound Function to decide computer or human winner of one round

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
 humanScore = 0;
-playerScore = 0;
+computerScore = 0;
 
 const getComputerChoice = () => {
   // Get a random choice of 0, 1, or 2
@@ -16,8 +16,33 @@ const getComputerChoice = () => {
 
 const getHumanChoice = () => {
   // Get choice from user
-  let choice = prompt("Enter rock, paper or scissors");
+  let choice = prompt("Enter rock, paper or scissors").toLowerCase();
   return choice;
 };
 
-console.log(getComputerChoice(), getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice == "rock" && computerChoice == "paper") {
+    console.log("You lose! Paper beats Rock!");
+    computerScore += 1;
+  } else if (humanChoice == "rock" && computerChoice == "scissors") {
+    console.log("You win! Rock beats Scissors!");
+    humanScore += 1;
+  } else if (humanChoice == "paper" && computerChoice == "rock") {
+    console.log("You win! Paper beats Rock!");
+    humanScore += 1;
+  } else if (humanChoice == "paper" && computerChoice == "scissors") {
+    console.log("You lose! Scissors beats Paper!");
+    computerScore += 1;
+  } else if (humanChoice == "scissors" && computerChoice == "rock") {
+    console.log("You lose! Rock beats Scissors!");
+    computerScore += 1;
+  } else if (humanChoice == "scissors" && computerChoice == "paper") {
+    console.log("You win! Scissors beats paper!");
+    humanScore += 1;
+  }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
